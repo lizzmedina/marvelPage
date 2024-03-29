ed9f88d874feca17560914c2add8318bed883868
Liza Medina <lizzmedina15@gmail.com>
2024-03-29 11:11:43 -0500

    build local running

--- pages/checkout/[id].page.tsx
+++ pages/checkout/[id].page.tsx
@@ -2,16 +2,39 @@ import { Grid, ThemeProvider } from "@mui/material";
 import FormPay from "dh-marvel/components/checkout/FormPay";
 import LayoutCheckout from "dh-marvel/components/layouts/layout-checkout";
 import { theme } from "dh-marvel/styles/material-theme";
-import { IComic } from "interface/comics";
 import { NextPage } from "next";
 
-const CheckoutPage: NextPage<{ comic: IComic }> = ({ comic }) => {
+const CheckoutPage: NextPage = () => {
 
     return(
         <ThemeProvider theme={theme}>
             <LayoutCheckout> 
                 <Grid container sx={{justifyContent:'center', alignContent:'center'}}>
-                    <FormPay />
+                    <FormPay defaultValues={{
+                        customer: {
+                            name: "",
+                            lastname: "",
+                            email: "",
+                            address: {
+                                address1: "",
+                                address2: null,
+                                city: "",
+                                state: "",
+                                zipCode: ""
+                            }
+                        },
+                        card: {
+                            number: "",
+                            cvc: "",
+                            expDate: "",
+                            nameOnCard: ""
+                        },
+                        order: {
+                            name: "",
+                            image: "",
+                            price: 0
+                        }
+                    }} />
                 </Grid>
             </LayoutCheckout>    
         </ThemeProvider>
